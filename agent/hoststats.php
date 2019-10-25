754fc7c69e7647d7821de7c036f86938943d06b0
Matthias <m@tth.es>
2019-10-25 19:33:28 +0200

    agent

--- agent/hoststats.php
+++ agent/hoststats.php
@@ -2,5 +2,17 @@
 <?php
 
 
+$clusterName = "arnold";
+
+
+while (1) {
+
+
+    $data = phore_exec(__DIR__ . "/source.d/system_info.sh");
+
+
+
+}
+
 
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -5,13 +5,4 @@ set -e
 apt-get update
 apt-get install -y gnupg2 wget
 
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-
-apt-get install -y docker.io apt-transport-https
-
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
-
-apt-get update
-
-sudo apt-get install -y metricbeat
 
